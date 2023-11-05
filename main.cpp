c37c0239b57747ecd049b32de6fbbd2b6d6a4abe
salma <salma.a.samei@gmail.com>
2023-11-05 19:27:35 +0200

    valid

--- main.cpp
+++ main.cpp
@@ -5,131 +5,171 @@
 #include <vector>
 #include <string>
 #include<sstream>
+#include <regex>
+bool valid=true;
 using namespace std;
 class BigReal
 {
 private:
-    string integer,fraction;
+    char sign ='+';
+    string integer,fraction,greal;
 public:
     BigReal()
     {
         return;
     }
-    BigReal(string a)
+    BigReal(string real)
     {
-        integer=a;
+        valid =true;
+        greal=real;
+            if (regex_match(real, regex("[+-]?\\d*.?\\d+")))
+            {
+                if (real[0] == '-') {
+                    sign = '-';
+                    real[0] = '0';
+                    real.erase(0,1);
+                }
+                else if (real[0] == '+') {
+                    sign = '+';
+                    real[0] = '0';
+                    real.erase(0,1);
+                }
+                if(real.find('.')!=-1)
+                {
+                    integer = real.substr(0, real.find('.'));
+                    fraction= real.substr( real.find('.')+1,real.size());
+                    if (fraction.size()==0)
+                        fraction="0";
+                    if (integer.size()==0)
+                        integer="0";
+                    if(sign=='+')
+                        greal=integer+"."+fraction;
+                    else
+                        greal=sign+integer+"."+fraction;
+                }
+                else
+                {
+                    greal=real;
+                }
+
+            }
+            else {
+                cout << "The Number is Not Valid!\n";
+                valid=false;
+            }
+
     }
     BigReal operator+(BigReal c2) {
         BigReal s3;
         string s0;
         int ch=0;
-        int i1 =integer.find(".");
-        int i2=c2.integer.find(".");
+        int i1 =greal.find(".");
+        int i2=c2.greal.find(".");
         if(i1>0&&i2<0)
         {
-            int no_after=integer.size()-(i1+1);
-            c2.integer=c2.integer+".";
+            int no_after=greal.size()-(i1+1);
+            c2.greal=c2.greal+".";
             for(int i=0;i<no_after;i++)
             {
-                c2.integer=c2.integer+"0";
+                c2.greal=c2.greal+"0";
             }
         }
         else if(i1<0&&i2>0)
         {
-            int no_after=c2.integer.size()-(i2+1);
-            integer=integer+".";
+            int no_after=c2.greal.size()-(i2+1);
+            greal=greal+".";
             for(int i=0;i<no_after;i++)
             {
-                integer=integer+"0";
+                greal=greal+"0";
             }
         }
         else if(i1>0&&i2>0)
         {
-            int no_after=c2.integer.size()-(i2+1);
-            int no_after1=integer.size()-(i2+1);
+            int no_after=c2.greal.size()-(i2+1);
+            int no_after1=greal.size()-(i2+1);
             if(no_after>no_after1)
                 for(int i=0;i<no_after-no_after1;i++)
                 {
-                    integer=integer+"0";
+                    greal=greal+"0";
                 }
             else
                 for(int i=0;i<no_after1-no_after;i++)
                 {
-                    c2.integer=c2.integer+"0";
+                    c2.greal=c2.greal+"0";
                 }
         }
-        if(integer[0]=='-'&&c2.integer[0]=='-')
+        if(greal[0]=='-'&&c2.greal[0]=='-')
         {
-            integer.erase(0,1);
-            c2.integer.erase(0,1);
+            greal.erase(0,1);
+            c2.greal.erase(0,1);
             ch=1;
         }
 
-        if(c2.integer.size()>integer.size())
+        if(c2.greal.size()>greal.size())
         {
-            int size =c2.integer.size()-integer.size();
+            int size =c2.greal.size()-greal.size();
             string zero;
             for(int i=0;i<size;i++)
                 zero=zero+"0";
-            integer=zero+integer;
+            greal=zero+greal;
         }
-        else if(c2.integer.size()<integer.size())
+        else if(c2.greal.size()<greal.size())
         {
-            int size =integer.size()-c2.integer.size();
+            int size =greal.size()-c2.greal.size();
             string zero;
             for(int i=0;i<size;i++)
                 zero=zero+"0";
-            c2.integer=zero+c2.integer;
+            c2.greal=zero+c2.greal;
 
         }
         int x,carry=0,n;
-        for(int i=integer.size()-1;i>=0;--i)
+        for(int i=greal.size()-1;i>=0;--i)
         {
-            if(integer[i]=='.') {
+            if(greal[i]=='.') {
                 --i;
-                s3.integer += ".";
+                s3.greal += ".";
             }
             if(i!=0) {
                 if(carry==1) {
-                    x = ((integer[i] - '0') + (c2.integer[i] - '0')+1);
+                    x = ((greal[i] - '0') + (c2.greal[i] - '0')+1);
                     carry=0;
                 }
                 else if(carry==0)
                 {
-                    x= ((integer[i] - '0') + (c2.integer[i] - '0'));
+                    x= ((greal[i] - '0') + (c2.greal[i] - '0'));
                 }
 
                 if (x < 10)
-                    s3.integer += to_string(x);
+                    s3.greal += to_string(x);
                 else {
                     x = x % 10;
-                    s3.integer += to_string(x);
+                    s3.greal += to_string(x);
                     carry=1;
                 }
             }
-            else if (i==0 && integer[i]!='-'&&c2.integer[i]!='-')
+            else if (i==0 && greal[i]!='-'&&c2.greal[i]!='-')
             {
 //                if(carry==1)
-                x = ((integer[i] - '0') + (c2.integer[i] - '0'));
+                x = ((greal[i] - '0') + (c2.greal[i] - '0'));
                 if(carry==1) {
                     x = x + 1;
                     carry=0;
                 }
                 s0=to_string(x);
                 reverse(s0.begin(),s0.end());
-                s3.integer += s0;
+                s3.greal += s0;
             }
 
         }
         if(ch==1)
-            s3.integer+="-";
-        reverse(s3.integer.begin(),s3.integer.end());
-        return s3.integer;
+            s3.greal+="-";
+        reverse(s3.greal.begin(),s3.greal.end());
+        return s3.greal;
 //
    }
     friend ostream&operator << (ostream&output,BigReal&s)
     {
-        output<<s.integer;
+        output<<s.greal;
         return output;
     }
 };
@@ -137,15 +177,13 @@ public:
 int main()
 {
     string no1,no2;
-    cin>>no1>>no2;
-   BigReal op(no1),op1(no2),op2;
-   op2=op+op1;
+    BigReal op2;
+        cout<<"please enter the first number\n";
+    cin>>no1;
+        BigReal op(no1);
+        cout<<"please enter the second number\n";
+   cin>>no2;
+        BigReal op1(no2);
+  op2=op+op1;
    cout <<op2;
-
-
-
-
 }
-
-
-
