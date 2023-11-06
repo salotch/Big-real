fdf0b314fe9be70ddaabc781e2840ecf868cb283
salma <salma.a.samei@gmail.com>
2023-11-06 02:40:51 +0200

    valid

--- library.h
+++ library.h
@@ -7,22 +7,121 @@
 #include <string>
 #include<sstream>
 #include <regex>
-#pragma once
+# pragma once
 bool valid=true;
 using namespace std;
 class BigReal
 {
 private:
-    char sign ='+';
     string integer,fraction,greal;
 public:
-    BigReal();
-
-    BigReal(string real);
+    char sign ='+';
+    BigReal();//empty constructor
+    BigReal(string real);//parameterize constructor to check if the number valid
     friend ostream&operator << (ostream&output,BigReal&s)
     {
         output<<s.greal;
         return output;
+    }
+    bool operator<(const BigReal& c2) {
+        int x,y;
+        int len1=integer.size();
+        int len2=c2.integer.size();
+        if (integer[0]=='-'&&c2.integer[0]!='-'){return true;}
+        if (c2.integer[0]=='-'&&integer[0]!='-')return false;
+        int count=0;
+        if(len1<len2)
+        {
+            return true;
+        }
+        else if (len1==len2)
+        {
+            for(int i =0;i<integer.size();i++)
+            {
+                if (integer[i]<c2.integer[i])
+                {
+                    return true;
+                    break;
+                }
+                else if (integer[i]<c2.integer[i])
+                {
+                    return false;
+                    break;
+                }
+                else
+                    count++;
+            }
+            if(count==integer.size())
+            {count=0;
+                for (int i =0;i<fraction.size();i++)
+                { if (fraction[i]<c2.fraction[i])
+                    {
+                        return true;
+                        break;
+                    }
+                    else if (fraction[i]>c2.fraction[i])
+                    {
+                        return false;
+                        break;
+                    }
+                    else
+                        count++;
+                }
+                if(count==fraction.size())
+                    return false;
+            }
+        }
+        return false;
+    }
+    bool operator>(const BigReal& c2) {
+        int x,y;
+        int len1=integer.size();
+        int len2=c2.integer.size();
+        if (integer[0]=='-'&&c2.integer[0]!='-'){return false;}
+        if (c2.integer[0]=='-'&&integer[0]!='-')return true;
+        int count=0;
+        if(len1>len2)
+        {
+            return true;
+        }
+        else if (len1==len2)
+        {
+            for(int i =0;i<integer.size();i++)
+            {
+                if (integer[i]>c2.integer[i])
+                {
+                    return true;
+                    break;
+                }
+                else if (integer[i]<c2.integer[i])
+                {
+                    return false;
+                    break;
+                }
+                else
+                    count++;
+            }
+            if(count==integer.size())
+            {count=0;
+                for (int i =0;i<fraction.size();i++)
+                { if (fraction[i]>c2.fraction[i])
+                    {
+                        return true;
+                        break;
+                    }
+                    else if (fraction[i]<c2.fraction[i])
+                    {
+                        return false;
+                        break;
+                    }
+                    else
+                        count++;
+                }
+                if(count==fraction.size())
+                    return false;
+            }
+        }
+        return false;
     }
     BigReal operator+(BigReal c2) {
         BigReal s3;
@@ -113,7 +212,132 @@ public:
         reverse(s3.greal.begin(), s3.greal.end());
         return s3.greal;
 //
-    };
+    }
+    BigReal operator-(BigReal c2) {
+        BigReal s3;
+        string s0;
+        int ch=0;
+        if(integer[0]!='-'&&c2.integer[0]=='-')
+        {
+            c2.integer.erase(0,1);
+            s3=operator+(c2);
+            return s3;
+        }
+        else if(integer[0]=='-'&&c2.integer[0]!='-')
+        {
+            c2.integer='-'+c2.integer;
+            s3=operator+(c2);
+            return s3;
+        }
+        int i1 =integer.find(".");
+        int i2=c2.integer.find(".");
+        if(i1>0&&i2<0)
+        {
+            int no_after=integer.size()-(i1+1);
+            c2.integer=c2.integer+".";
+            for(int i=0;i<no_after;i++)
+            {
+                c2.integer=c2.integer+"0";
+            }
+        }
+        else if(i1<0&&i2>0)
+        {
+            int no_after=c2.integer.size()-(i2+1);
+            integer=integer+".";
+            for(int i=0;i<no_after;i++)
+            {
+                integer=integer+"0";
+            }
+        }
+        else if(i1>0&&i2>0)
+        {
+            int no_after=c2.integer.size()-(i2+1);
+            int no_after1=integer.size()-(i2+1);
+            if(no_after>no_after1)
+                for(int i=0;i<no_after-no_after1;i++)
+                {
+                    integer=integer+"0";
+                }
+            else
+                for(int i=0;i<no_after1-no_after;i++)
+                {
+                    c2.integer=c2.integer+"0";
+                }
+        }
+        if(integer[0]=='-'&&c2.integer[0]=='-')
+        {
+            string swap;
+            integer.erase(0,1);
+            c2.integer.erase(0,1);
+            c2.integer.swap(integer);
+        }
+//
+        if(c2.integer.size()>integer.size())
+        {
+            int size =c2.integer.size()-integer.size();
+            string zero;
+            for(int i=0;i<size;i++)
+                zero=zero+"0";
+            integer=zero+integer;
+        }
+        else if(c2.integer.size()<integer.size())
+        {
+            int size =integer.size()-c2.integer.size();
+            string zero;
+            for(int i=0;i<size;i++)
+                zero=zero+"0";
+            c2.integer=zero+c2.integer;
+
+        }
+        for(int i=0;i<integer.size();i++)
+        {
+            if(integer[i]=='.')
+                ++i;
+            if(integer[i]-'0'<c2.integer[i]-'0')
+            {
+                ch=1;
+                c2.integer.swap(integer);
+            }
+        }
+        int x,borrow=0,n;
+        for(int i=integer.size()-1;i>=0;--i) {
+            if (integer[i] == '.') {
+                --i;
+                s3.integer += ".";
+            }
+            if(integer[i]<c2.integer[i])
+            {
+                if(borrow==1) {
+                    x = (10 + (integer[i] - '0') - (c2.integer[i] - '0')-1);
+                    borrow=0;
+                }
+                else {
+                    x = (10 + (integer[i] - '0') - (c2.integer[i] - '0'));
+                }
+                s3.integer+=to_string(x);
+                borrow=1;
+
+            }
+            else
+            {
+                if(borrow==1)
+                {
+                    x=((integer[i]-'0')-(c2.integer[i]-'0')-1);
+                    borrow=0;
+
+                }
+                else
+                    x=((integer[i]-'0')-(c2.integer[i]-'0'));
+                s3.integer+=to_string(x);
+            }
+        }
+        if(ch==1)
+            s3.integer+="-";
+        reverse(s3.integer.begin(),s3.integer.end());
+        return s3.integer;
+
+
+    }
 };
 
 BigReal::BigReal()
@@ -121,13 +345,11 @@ BigReal::BigReal()
     return;
 }
 BigReal :: BigReal(string real) {
-    valid = true;
-    greal = real;
-    if (regex_match(real, regex("[+-]?\\d*.?\\d+"))) {
+    valid = true;//to check if the number is valid or not
+    if (regex_match(real, regex("[+-]?\\[0-9]*.?\\[0-9]+"))) //using regular expression i have to check tht the first index have +or-or neither after that digits after that have at most dot after that digits
+        {
         if (real[0] == '-') {
-            sign = '-';
-            real[0] = '0';
-            real.erase(0, 1);
+            sign = '-';//here to split the real
         } else if (real[0] == '+') {
             sign = '+';
             real[0] = '0';
@@ -140,15 +362,16 @@ BigReal :: BigReal(string real) {
                 fraction = "0";
             if (integer.size() == 0)
                 integer = "0";
-            if (sign == '+')
-                greal = integer + "." + fraction;
-            else
-                greal = sign + integer + "." + fraction;
+            greal = integer + "." + fraction;//to save the big real after make it suitable
+
         } else {
-            greal = real;
+            greal = real;//if the number does not have dot
+            integer=real;
         }
 
-    } else {
+    }
+    else//if the number not valid
+    {
         cout << "The Number is Not Valid!\n";
         valid = false;
     }
--- main.cpp
+++ main.cpp
@@ -1,29 +1,50 @@
 #include "library.h"
 using namespace std;
 int main()
-{char choice;
+{string no1,no2;
+    char choice;
     cout<<"                    Welcome to BigReal Operations\n"<<"Enter the operation you want to do on Two-BigReal numbers\n"
     <<"+\n"<<"-\n"<<"<\n"<<">\n"<<"=\n"<<"your choice is: ";
     cin>>choice;
-    string no1,no2;
     do
     {
-    cout<<"please enter the first number:\n";
-    cin>>no1;
+    cout<<"please enter the first number: ";
+    cin.ignore();
+    getline(cin,no1);
     BigReal t1(no1);
     }while (!valid);
-    do
-    {
+ do{
     cout<<"please enter the second number:\n";
-    cin>>no2;
+     cin.ignore();
+        getline(cin,no2);
     BigReal t2(no2);
-    }while (!valid);
+    }  while (!valid);
+
     BigReal op2,op(no1),op1(no2);
     if(choice=='+')
     {
-    op2=op+op1;
-    cout<<"Summation = ";
-    cout <<op2;
+        op2 = op + op1;
+        cout << "Summation = ";
+        cout << op2;
     }
-
+    else if(choice=='>')
+    {
+       if(op.sign==op1.sign&&op.sign=='-')
+           cout<<((op > op1)? "No" : "Yes");
+       else
+        cout<<((op > op1)? "Yes" : "No");
+    }
+    else if(choice=='<')
+    {
+        if(op.sign==op1.sign&&op.sign=='-')
+            cout<<((op < op1)? "No" : "Yes");
+        else
+            cout<<((op < op1)? "Yes" : "No");
+    }
+    else if(choice=='-')
+    {
+        op2 = op - op1;
+        cout << "Difference = ";
+        cout << op2;
+    }
 }
